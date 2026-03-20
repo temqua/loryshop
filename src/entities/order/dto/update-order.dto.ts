ce9a456d96ef77282bee8274eed9d2d8afec2070
artem <art_nazarov@mail.ru>
2026-03-20 04:46:11 +0300

    validation

--- src/entities/order/dto/update-order.dto.ts
+++ src/entities/order/dto/update-order.dto.ts
@@ -1,11 +1,15 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateOrderDto } from './create-order.dto';
 import { OrderStatus, PaymentStatus } from '@prisma/client';
+import { IsString } from 'class-validator';
 
 export class UpdateOrderDto extends PartialType(CreateOrderDto) {
+  @IsString()
   status: OrderStatus;
 
+  @IsString()
   paymentStatus: PaymentStatus;
 
+  @IsString()
   cancelReason: string;
 }
